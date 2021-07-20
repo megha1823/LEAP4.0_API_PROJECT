bbe727cf09e0903ff3b2006e7e740fcd8fb8dcf0
MOHTADIN KHAN <83912857+M-khan710@users.noreply.github.com>
2021-07-20 22:11:22 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var request = new XMLHttpRequest()
 
-request.open('GET','https:api.github.com/users/megha1823/repos',true)
 
-request.onload = function(){
-    var data = JSON.parse(this.response);
-    console.log(data);
+request.open('GET', 'https://api.github.com/users/megha1823/repos', true)
+
+request.onload = function () {
+  
+  var data = JSON.parse(this.response);
+
+  var statusHTML = '';
+  $.each(data, function(i, status) {
+    statusHTML += '<tr>';
+    statusHTML += '<td>' + status.id + '</td>';
+    statusHTML += '<td>' + status.name + '</td>';
+    statusHTML += '<td>' + status.html_url + '</td>';
+    statusHTML += '<td>' + status.lanuage + '</td>';
+    statusHTML += '</tr>';
+  });
+  $('tbody').html(statusHTML);
 }
+
 
 request.send();
